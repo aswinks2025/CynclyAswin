11bef177ad507eb2cba12a695a872a1a9fb2fcd1
aswinks2025 <aswinks2025@ec.ajce.in>
2026-01-21 10:37:07 +0530

    calculator

---
+++ A08_Doubleca;culator/Program.cs
@@ -0,0 +1,11 @@
+﻿
+double i, j, result = 0;
+
+Console.Write("Value 1  : ");
+i = double.Parse(Console.ReadLine());
+
+Console.Write("Value 2 : ");
+j = double.Parse(Console.ReadLine());
+
+result = i + j;
+Console.WriteLine(result);
---
+++ A11_iFeLSE/Program.cs
@@ -0,0 +1,12 @@
+﻿string city = string.Empty;
+Console.WriteLine("Enter City :")
+
+    city = Console.ReadLine();
+
+if(city == "Kochi")
+{
+    Console.WriteLine("Queeen of Arabian Sea");
+}
+elseif(city == "Kottayam"){
+    Console.WriteLine("Queeen of Arabian Sea");
+}
